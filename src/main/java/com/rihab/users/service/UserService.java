d969a81cea2ba0e67cff413f93b4780506b65bf7
Rihab Zouaoui <146938064+RihabZ@users.noreply.github.com>
2023-11-28 22:26:47 +0100

    Add files via upload

---
+++ src/main/java/com/rihab/users/service/UserService.java
@@ -0,0 +1,19 @@
+package com.rihab.users.service;
+
+import java.util.List;
+
+import com.rihab.users.entities.Role;
+import com.rihab.users.entities.User;
+
+public interface UserService {
+	
+User saveUser(User user);
+
+User findUserByUsername (String username);
+
+Role addRole(Role role);
+
+User addRoleToUser(String username, String rolename);
+List<User> findAllUsers();
+
+}
